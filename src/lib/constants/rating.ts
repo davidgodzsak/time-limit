4f072d2e54dd1c1b1581bbb4e7eb1bc42e387059
Dávid Godzsák <godzsakdavid@gmail.com>
2026-04-30 20:53:07 +0200

    Fix broken firefox rate link

--- src/lib/constants/rating.ts
+++ src/lib/constants/rating.ts
@@ -1,8 +1,8 @@
 declare const __BROWSER_TARGET__: string;
 
 const RATING_URLS: Record<string, string> = {
-  firefox: 'https://addons.mozilla.org/firefox/addon/time-limit/reviews/',
-  chrome: 'https://chromewebstore.google.com/detail/time-limit/fhlebfafkdeiclbanmjjojjijdgllbbf/reviews',
+  firefox: 'https://addons.mozilla.org/firefox/addon/time-limit-extension/reviews/',
+  chrome: 'https://chromewebstore.google.com/detail/mindful-browse/fhlebfafkdeiclbanmjjojjijdgllbbf/reviews',
 };
 
 export function getRatingUrl(): string {
